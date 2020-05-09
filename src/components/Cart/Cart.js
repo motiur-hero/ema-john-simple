ce1a0429efd1b8d68e8bd6310af213bd65b11401
motiur-hero <motiurgreenpower@gmail.com>
2020-05-09 04:59:26 +0300

    shoping cart done

---
+++ src/components/Cart/Cart.js
@@ -0,0 +1,37 @@
+import React from 'react';
+
+const Cart = (props) => {
+
+    const cart = props.cart
+    console.log(cart)
+    //const total = cart.reduce((total,prd) => total + prd.price,0)
+    let total = 0
+    for (let i = 0; i < cart.length; i++) {
+        const prd = cart[i];
+        total =total+prd.price
+        
+    }
+    let shipping = 0
+    if(total > 35){
+        shipping= 0;
+    }
+    else if(total > 15){
+        shipping = 4.99
+    }
+    else if(total > 0){
+        shipping= 12.99
+    }
+    const tax = (total*.10).toFixed(2);
+    const grandTotal = (total + Number(tax) + shipping).toFixed(2)
+    return (
+        <div>
+            <h4>Order Summery :</h4>
+            <p>Items Orderd:{cart.length}</p>
+            <p><small>Vat & Tax : {tax}</small></p>
+            <p><small>shipping cost: {shipping}</small></p>
+            <p>Total Price: {grandTotal}</p>
+        </div>
+    );
+};
+
+export default Cart;
---
+++ src/components/Product/Product.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import './Product.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faShoppingCart } from '@fortawesome/free-solid-svg-icons'
+
+
+const Product = (props) => {
+    //console.log(props)
+    const { name, price, seller, img, stock } = props.product
+    return (
+        <div className="product">
+            <div>
+                <img src={img} alt="" />
+            </div>
+            <div>
+                <h4  className="product-name">{name}</h4>
+                <br/>
+                <p><small>by:{seller}</small></p>
+                
+                <p> $ {price}</p>
+            
+                <p><small>only {stock} let in stock- Order soon</small></p>
+                <button onClick={()=>props.handleAddProduct(props.product)} 
+                className="add-button">
+                    <FontAwesomeIcon icon={faShoppingCart} />
+                    add to cart
+                    </button>
+            </div>
+
+        </div>
+    );
+};
+
+export default Product;
